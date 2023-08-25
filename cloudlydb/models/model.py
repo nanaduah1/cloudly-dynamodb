c50d9e3d98fde037352338db8801aaec20347d97
Nana Duah <gabano2005@gmail.com>
2023-08-25 08:50:30 -0400

    Implemented create interface

--- cloudlydb/models/model.py
+++ cloudlydb/models/model.py
@@ -6,26 +6,52 @@ from uuid import uuid4
 from cloudlydb.core.dynamodb import PutItemCommand
 
 
+class UnknownFieldException(Exception):
+    def __init__(self, message: str, *fields):
+        super().__init__(f"{message} Bad field(s): {fields}")
+
+
 def _fully_qualified_name(cls):
     return cls.__module__ + "." + cls.__name__
 
 
 class ItemManager:
     def __init__(self):
-        self._class = None
+        self._model_class = None
+        self._instance = None
 
     def __get__(self, instance, owner):
-        self._class = owner
+        self._model_class = owner
+        if instance:
+            self._instance = instance
+
         return self
 
-    def create(self, pk: str, sk: str, **kwargs):
+    def __set__(self, instance, value):
+        raise ValueError("Cannot set ItemManager")
+
+    def create(self, **kwargs):
         """
         Create a new item in the database using config from the attached model class
         """
 
-        cleaned_data = self.__validate_data(pk, sk, kwargs)
+        cleaned_data = self.__validate_data(**kwargs)
+
+        instance = self._instance or self._model_class(**cleaned_data)
+
+        sk = instance._create_sk()
+        pk = instance._create_pk()
+
+        assert pk is not None, "pk must be provided"
+        assert sk is not None, "sk must be provided"
+
+        # Make sure we have an id
+        cleaned_data["id"] = instance.id or self._model_class._new_id()
+
         create_command = PutItemCommand(
-            self._class.Meta.dynamo_table, data=cleaned_data, key=dict(pk=pk, sk=sk)
+            self._model_class.Meta.dynamo_table,
+            data=cleaned_data,
+            key=dict(pk=pk, sk=sk),
         )
         response = create_command.execute()
 
@@ -34,37 +60,36 @@ class ItemManager:
 
         _data = {**cleaned_data}
 
-        # Extract the id from the data being used to initialize the model
+        # Exclude the id from the data being used to initialize the model
         # since it is not part of the model's dataclass fields
+
         id = _data.pop("id")
-        new_item = self._class(**_data)
+        new_item = self._model_class(**_data)
         new_item.__dict__["_pk"] = pk
         new_item.__dict__["_sk"] = sk
-        new_item.__dict__["id"] = id
+        new_item.id = id
 
         return new_item
 
-    def __validate_data(self, pk: str, sk: str, kwargs: dict) -> dict:
-        assert self._class is not None, "ModelManager must be attached to a model class"
-        assert pk is not None, "pk must be provided"
-        assert sk is not None, "sk must be provided"
+    def __validate_data(self, **kwargs) -> dict:
+        assert (
+            self._model_class is not None
+        ), "ModelManager must be attached to a model class"
 
         assert (
-            self._class.Meta.dynamo_table is not None
-        ), f"dynamo_table must be set in {self._class.__name__}'s Meta class"
+            self._model_class.Meta.dynamo_table is not None
+        ), f"dynamo_table must be set in {self._model_class.__name__}'s Meta class"
 
         # Get all fields from the model class. Exclude private fields
         public_fields = tuple(f for f, _ in kwargs.items() if not f.startswith("_"))
-        all_fields = tuple(f for f, _ in self._class.__dataclass_fields__.items())
+        all_fields = tuple(f for f, _ in self._model_class.__dataclass_fields__.items())
 
         # Check that all fields in kwargs are in the model class
         bad_fields = tuple(
             f for f in public_fields if f not in all_fields and f != "id"
         )
         if bad_fields:
-            raise Exception(
-                f"{self._class.__name__} does not contain fields: {bad_fields}"
-            )
+            raise UnknownFieldException("Unexpected field(s).", bad_fields)
 
         return {k: v for k, v in kwargs.items() if k in public_fields or k == "id"}
 
@@ -85,18 +110,35 @@ class ItemManager:
         pass
 
 
+class IdField:
+    """
+    A descriptor that auto-generates an id for a model
+    """
+
+    def __init__(self):
+        self.name = None
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        value = instance.__dict__.get(self.name)
+        return value
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+
 @dataclass
 class DynamodbItem(ABC):
+    id = IdField()
     items = ItemManager()
 
     def save(self):
-        if self.id is None:
-            self.__dict__["id"] = f"{datetime.now().timestamp()}{uuid4()}"
-
-        sk = self._create_sk()
-        pk = self._create_pk()
-
-        self.items.create(pk=pk, sk=sk, **self.__dict__)
+        res = self.items.create(**self.__dict__)
+        self.id = res.id
 
     def delete(self):
         pk = self.__dict__.get("_pk")
@@ -117,6 +159,6 @@ class DynamodbItem(ABC):
         self.__dict__["_sk"] = sk
         return sk
 
-    @property
-    def id(self):
-        return self.__dict__.get("id")
+    @classmethod
+    def _new_id(cls):
+        return f"{datetime.now().timestamp()}{uuid4()}"
--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from cloudlydb.models.model import DynamodbItem
+import pytest
+
+from cloudlydb.models.model import DynamodbItem, UnknownFieldException
 
 
 class FakeCreateTable:
@@ -32,6 +34,7 @@ def test_id_auto_generates():
     assert item["data"]["id"] == model.id
     assert item["data"]["name"] == "test"
     assert item["data"]["age"] == 10
+    assert item["data"]["id"] is not None
 
 
 def test_id_is_unique():
@@ -50,3 +53,42 @@ def test_id_is_unique():
     model1.save()
     model2.save()
     assert model1.id != model2.id
+
+
+def test_create_record_from_manager():
+    table = FakeCreateTable()
+
+    @dataclass
+    class MyFakeModel(DynamodbItem):
+        name: str
+        age: int
+
+        class Meta:
+            dynamo_table = table
+
+    model = MyFakeModel.items.create(name="test", age=10)
+    assert model.id is not None
+    assert len(table.items) == 1
+    item = table.items[0]
+
+    assert item["pk"] == model._pk
+    assert item["sk"] == model._sk
+    assert item["data"]["id"] == model.id
+    assert item["data"]["name"] == "test"
+    assert item["data"]["age"] == 10
+    assert item["data"]["id"] is not None
+
+
+def test_raises_exception_when_field_is_not_on_model():
+    table = FakeCreateTable()
+
+    @dataclass
+    class MyFakeModel(DynamodbItem):
+        name: str
+        age: int
+
+        class Meta:
+            dynamo_table = table
+
+    with pytest.raises(UnknownFieldException):
+        MyFakeModel.items.create(last_name="test", age=10)
