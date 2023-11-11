699ddadf83392c62723ba318999b1d35260438bf
Nana Duah <gabano2005@gmail.com>
2023-11-11 09:39:35 -0500

    Add support for update conditionals

--- tests/test_update_command.py
+++ tests/test_update_command.py
@@ -1,4 +1,20 @@
-from cloudlydb.core.dynamodb import SetExpression
+from dataclasses import dataclass
+from cloudlydb.core.dynamodb import (
+    SetExpression,
+    UpdateItemCommand,
+    ConditionalExecuteMixin,
+    PutItemCommand,
+)
+import pytest
+
+
+@dataclass
+class FakeDb:
+    data: dict = None
+
+    def update_item(self, **kwargs):
+        self.data = kwargs
+        return {}
 
 
 def test_update_simple_object():
@@ -55,3 +71,72 @@ def test_update_object_with_inner_inner_map():
         update_expr
         == "SET #name = :name, #car.#carnum = :carnum, #car.#carvin = :carvin, #car.#own.#carownname = :carownname, #car.#own.#carownage = :carownage"
     )
+
+
+def test_update_command_with_condition_umet_condition(put_item, db_table):
+    put_item({"pk": "1234", "sk": "4567", "data": {"name": "nana", "age": 10}})
+
+    tested = UpdateItemCommand(
+        database_table=db_table,
+        key={"pk": "1234", "sk": "4567"},
+        data={"name": "nana", "age": 10},
+        condition_expression="attribute_exists(#d.#agent.#name)",
+        condition_expression_attr_names={
+            "#d": "data",
+            "#agent": "agent",
+            "#name": "name",
+        },
+    )
+
+    with pytest.raises(ConditionalExecuteMixin.ConditionUnmetError):
+        response = tested.execute()
+        assert response is not None
+        print(response)
+
+
+def test_update_command_with_condition_met_condition(put_item, db_table):
+    put_item({"pk": "1234", "sk": "4567", "data": {"name": "nana", "age": 10}})
+
+    tested = UpdateItemCommand(
+        database_table=db_table,
+        key={"pk": "1234", "sk": "4567"},
+        data={"name": "nana", "age": 10},
+        condition_expression="attribute_exists(#d.#name)",
+        condition_expression_attr_names={
+            "#d": "data",
+            "#name": "name",
+        },
+    )
+
+    response = tested.execute()
+    assert response is not None
+    assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+
+def test_put_item_command_with_condition_unmet(put_item, db_table):
+    put_item({"pk": "1234", "sk": "4567", "data": {"name": "nana", "age": 10}})
+
+    tested = PutItemCommand(
+        database_table=db_table,
+        key={"pk": "1234", "sk": "4567"},
+        data={"name": "nana", "age": 10},
+        condition_expression="attribute_not_exists(pk) AND attribute_not_exists(sk)",
+    )
+
+    with pytest.raises(ConditionalExecuteMixin.ConditionUnmetError):
+        response = tested.execute()
+        assert response is not None
+        print(response)
+        assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+
+def test_put_item_with_met_condition(db_table):
+    tested = PutItemCommand(
+        database_table=db_table,
+        key={"pk": "3333", "sk": "4444567"},
+        data={"name": "nana", "age": 10},
+        condition_expression="attribute_not_exists(pk) AND attribute_not_exists(sk)",
+    )
+
+    response = tested.execute()
+    assert response is not None
